7af78446e72860df834dfed9cbd688e83d1db90b
anshu <manu22425@gmail.com>
2024-09-10 19:32:53 +0530

    Login Frame done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CameraVIew from './components/CameraVIew';
 import Header from './components/Header';
+import Login from './components/Login';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <>
       <Header />
-      <CameraVIew />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/gallery' element={<Gallery />} />
+          <Route exact path='/camera' element={<CameraVIew />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email: ", email);
+    console.log("password: ", password);
+  }
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md bg-white shadow-md rounded-lg p-8'>
+        <h3 className='text-2xl font-bold mb-6 text-center'>Login</h3>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2'>
+              Email
+            </label>
+            <input
+              type='email'
+              id='email'
+              className='w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+
+    </div>
+  );
+};
+
+export default Login;
