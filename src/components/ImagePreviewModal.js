517f975cad97896c5961bb105ad8d5926a0c2416
anshu <manu22425@gmail.com>
2024-10-01 00:34:31 +0530

    Made Image Preview Modal Scrollable and added 8 images

--- src/components/ImagePreviewModal.js
+++ src/components/ImagePreviewModal.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ImagePreviewModal = ({ selectedImages, isSelectMode, handlePrint, onClose }) => {
   return (
     <div className='fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-40'>
-      <div className="w-1/2 bg-white shadow-lg p-4 transition-transform transform translate-x-0 border z-50">
+      <div className="w-1/2 bg-white shadow-lg p-4 transition-transform transform translate-x-0 border z-50 max-h-[90vh] overflow-y-auto">
         <div className='flex mt-2'>
           <h2 className="text-lg font-semibold mb-4">Selected Images</h2>
           <div className='ms-auto'>
@@ -24,9 +24,14 @@ const ImagePreviewModal = ({ selectedImages, isSelectMode, handlePrint, onClose 
         </div>
         <div id="image-grid" className="grid grid-cols-2 gap-3">
           {selectedImages.map((image, index) => (
-            <div key={index} className="w-full h-auto">
-              <img src={image} alt={`Selected ${index + 1}`} className="w-full h-auto" />
-            </div>
+            <>
+              <div key={index} className="w-full h-auto">
+                <img src={image} alt={`Selected ${index + 1}`} className="w-full h-auto" />
+              </div>
+              <div key={index} className="w-full h-auto">
+                <img src={image} alt={`Selected ${index + 1}`} className="w-full h-auto" />
+              </div>
+            </>
           ))}
         </div>
       </div>
